fba92b4e8cb8470ce28d1b12014fb9821deaeef2
Oneline <leon.dahmer@gmail.com>
2018-01-05 21:10:32 -0500

    create and show actions plus templates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,19 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Your Message was successfully created"
+      redirect_to article_path(@article) 
+    else
+      render 'new'
+    end
+    
   end
   
   def show
-    
-  end
+    @article = Article.find(params[:id])
+  end 
   
   def update
     
