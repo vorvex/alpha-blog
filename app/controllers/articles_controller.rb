7d9e8be1be93ea973a6ad38e29ae0a3d4d6590da
Oneline <leon.dahmer@gmail.com>
2018-01-12 13:39:13 -0500

    many associations for user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,13 +14,14 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Your Message was successfully created"
       redirect_to article_path(@article) 
     else
       render 'new'
     end
-    
+     
   end
   
   def show
